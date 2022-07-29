00de8d2d5c939cd8fda24dc3d7e197b041313e48
mariajosefina <mariatoivanen@outlook.com>
2022-07-29 15:57:18 +0300

    weekly tasks done

--- Learning diary Maria Toivanen/wwwroot/js/site.js
+++ Learning diary Maria Toivanen/wwwroot/js/site.js
@@ -9,10 +9,31 @@ function ConfirmDeletion() {
     let confirmAction = confirm("Are you sure you want to delete this topic?");
 
     if (confirmAction) {
-        alert('Topic was saved to the database.');
+        alert('Topic was deleted from the database.');
     }
     else {
+        event.preventDefault();
         alert('Deletion cancelled.');
     }
 
 };
+
+var color = document.querySelectorAll(".form-control");
+color.forEach(color => color.addEventListener("focus", function () { color.style.backgroundColor = "#ffff99" }));
+color.forEach(color => color.addEventListener("blur", function () { color.style.backgroundColor = ""}));
+
+
+
+/*
+function changeColorOnMouseDown() {
+
+   
+    color.addEventListener('focus', style.backgroundColor = "#ffff99");
+};
+
+
+function changeColorOnMouseUp() {
+    var color = document.getElementById("formColor")
+    color.addEventListener('blur', style.backgroundColor = "#FFFFFF");
+    };
+    */
